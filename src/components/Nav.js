6040c544fcfe0d4b8f0c5ef39b8166b7f0b236e9
KamauIan <kamauiyan@gmail.com>
2018-09-26 17:06:37 +0300

    Added link to Resume

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => (
   <div className="header" style={{paddingTop:"50px"}}>
     <ul className="nav justify-content-center">
   <li className="nav-item">
-    <a className="nav-link text-white" style={{fontSize:"18px", fontWeight:700}} href="#">RESUME</a>
+    <a className="nav-link text-white" style={{fontSize:"18px", fontWeight:700}} href="http://bit.ly/WaweruResume" target="_blank">RESUME</a>
   </li>
   <li className="nav-item">
     <a className="nav-link text-white" style={{fontSize:"18px", fontWeight:700}} href="#portfolio">PORTFOLIO</a>
